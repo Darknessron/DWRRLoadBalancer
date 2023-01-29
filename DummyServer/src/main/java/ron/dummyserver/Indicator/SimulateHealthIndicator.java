c2b53f5e5d732708651c841918762b89e19ef959
Ron <cg12192001@gmail.com>
2023-01-29 23:42:53 +0800

    Add Health Indicator to simulate health status

---
+++ DummyServer/src/main/java/ron/dummyserver/Indicator/SimulateHealthIndicator.java
@@ -0,0 +1,30 @@
+package ron.dummyserver.Indicator;
+
+import java.util.Random;
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.boot.actuate.health.HealthIndicator;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SimulateHealthIndicator implements HealthIndicator {
+
+  private final Random random = new Random();
+
+  @Override
+  public Health getHealth(boolean includeDetails) {
+    return health();
+  }
+
+  @Override
+  public Health health() {
+    int i = random.nextInt(10);
+    Health health = switch (i) {
+      case 0, 1, 2 -> Health.down().build();
+      case 3, 4, 5, 6 -> Health.up().build();
+      case 7, 8 -> Health.outOfService().build();
+      case 9 -> Health.unknown().build();
+      default -> throw new IllegalStateException("Unexpected value: " + i);
+    };
+    return health;
+  }
+}
