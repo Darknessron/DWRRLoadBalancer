5b3978113ecd75a6ecdcade26dac599fba758e94
Ron <cg12192001@gmail.com>
2023-01-29 17:37:01 +0800

    Add check mechanism to prevent endless loop

--- DynamicWeightedRoundRobinLoadBalancer/src/main/java/ron/dwrrlb/controller/DWRRController.java
+++ DynamicWeightedRoundRobinLoadBalancer/src/main/java/ron/dwrrlb/controller/DWRRController.java
@@ -1,5 +1,6 @@
 package ron.dwrrlb.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.IOException;
@@ -15,6 +16,7 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.locks.ReentrantReadWriteLock;
+import java.util.stream.IntStream;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -39,42 +41,69 @@ public class DWRRController {
   private final List<ServerNode> availableServers = new ArrayList<>();
   private final List<ServerNode> unavailableServers = new ArrayList<>();
 
-  @PostMapping
+  @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
   public ResponseEntity<JsonNode> loadBalance(@RequestBody JsonNode jsonNode)
       throws URISyntaxException, IOException, InterruptedException {
     if (count == Long.MAX_VALUE) count = 0;
     ServerNode node;
     lock.readLock().lock();
     int serverSize = availableServers.size();
+    log.info("{} nodes available", serverSize);
     try {
       int index = (int) (count % serverSize);
       node = availableServers.get(index);
     }finally {
       lock.readLock().unlock();
     }
-    // Loading too heavy, skip to next node.
-    if (node.getWeight() < 50 && serverSize != 1)  {
-      count++;
-      loadBalance(jsonNode);
-    }
 
-    HttpRequest req = HttpRequest.newBuilder().uri(new URI(node.getAddress() + node.getUri())).POST(
-        BodyPublishers.ofString(jsonNode.toString())).build();
+    JsonNode result;
+    // Loading too heavy, skip to less loading node.
+    if (node.getWeight() < 50 && serverSize != 1)  {
+      log.info("node {} loading too heavy, skip to next node", node.getServerName());
+
+      //Check other nodes' weight
+      int index = IntStream.range(0, availableServers.size()).filter(i -> availableServers.get(i).getWeight() >= 50).findFirst().orElse(-1);
+
+      if (index >= 0 ) {
+        //All nodes are busy, follow original order.
+        result = processRequest(node, jsonNode);
+      }else {
+        //Jump to less loading node
+        result = processRequest(availableServers.get(index), jsonNode);
+      }
+    }
+    //Loading is normal, process the request
+    result = processRequest(node, jsonNode);
+    return ResponseEntity.ok(result);
+  }
+
+  private JsonNode processRequest(ServerNode node, JsonNode jsonNode)
+      throws IOException, URISyntaxException, InterruptedException {
+
+    HttpRequest req = HttpRequest.newBuilder().uri(new URI(node.getAddress() +"/"+ node.getUri()))
+        .header("Content-Type", "application/json")
+        .POST(BodyPublishers.ofString(jsonNode.toString())).build();
     long startMillisecond = System.currentTimeMillis();
     String response = client.send(req, BodyHandlers.ofString()).body();
     long endMillisecond = System.currentTimeMillis();
     JsonNode resp = new ObjectMapper().readTree(response);
 
     long processTime = endMillisecond - startMillisecond;
-    // Process speed less than 2 seconds, increase the weight
-    if (processTime < 20000) {
-      if (node.getWeight() != 100) node.setWeight(node.getWeight() * 2);
-    } else if (processTime > 30000) {
+    log.info("Node:{}  Process Time: {} ms, weight: {}", node.getServerName(), processTime, node.getWeight());
+    // Process speed less than 2 seconds, resume the weight to 100
+    if (processTime < 2000) {
+      node.setWeight(100);
+    } else if (processTime > 3000) {
       // Process speed more than 3 seconds, decrease the weight
       node.setWeight(node.getWeight() / 2);
     }
-
-    return ResponseEntity.ok(resp);
+    lock.writeLock().lock();
+    try {
+      count++;
+    }finally {
+      lock.writeLock().unlock();
+    }
+    return resp;
   }
 
   @PostMapping(value = "/register", consumes = MediaType.APPLICATION_JSON_VALUE)
